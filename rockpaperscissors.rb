f41145934374a70f210287f5dd5c07feba3a99f2
Skyler Sidner <malevolentdragon@gmail.com>
2015-01-30 11:53:12 -0600

    Completed game logic methods

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -4,62 +4,93 @@
 # Intro
 puts "Welcome to Sky's Rock/Paper/Scissors Game!  This is for two players, and winner takes all."
 
+
+class Driver
+  #This class should pull everything to run your program instance
+  
+  
+  def new_game(*players)
+    rps = RockPaperScissors.new(players)
+  end
+  
+  def new_match(number_of_games, *players)
+    x = number_of_games
+    for 1..x do
+      new_game(players)
+    end
+    # @who_is_playing_match = Hash.new
+    # @who_is_playing_game.each |player| do
+    #   @who_is_playing_match[player] = 0
+    # end
+  end
+  
+  def winner_of_match
+    # Compare games to determine overall winners of the match
+  end
+  
+  def format_output
+    # Self explanatory
+  end
+  
+  
+end
+
+
 class Player
   
-  attr_accessor :name, :score
+  attr_accessor :name, :score, :move
   
   def initialize(name)
     @name = name
-    @moves = Array.new
+    @move = ""
     @score = 0
-  end
-  
-  def track_move(move)
-    @moves << move
   end
 end
 
-class Game
+class RockPaperScissors
   
-  attr_accessor :games_played
+  attr_accessor :games_played, :winner
   
   def initialize(*players)
     @games_played = 0
-    @who_is_playing = Hash.new
-    players.each |player| do
-      @who_is_playing[player] = 0
-    end
+    @who_is_playing_game = Arary.new
     rules_of_game
+    @winner = ""
   end
   
   def rules_of_game
     rules = Hash.new
     rules {"Rock" => "Scissors", "Paper" => "Rock", "Scissors" => "Paper"}
   end
-  
-  def new_game
-    
+
+  def capture_player_moves
+    @who_is_playing_game.each do |x|
+      player_move(x)
+    end
   end
-  
-  def player_move
-    
+
+  def player_move(player_x)
+    move = ""
+    while rules.has_key?(move) = false do
+      puts "Player #{player_x}, choose your move (Rock, Paper, Scissors): "
+      move = gets.chomp
+    end
+    for player_x.name do
+      player_x.move = move
+    end
   end
   
   def winner_of_game
-    
+    if rules[@who_is_playing[0].move] == @who_is_player[1].move
+      @winner = @who_is_playing[0]
+    elsif  rules[@who_is_playing[1].move] == @who_is_player[0].move
+      @winner = @who_is_playing[1]
+    else
+      @winner = "Tie"
+    end
   end
   
-  def new_match
-    
-  end
-  
-  def winner_of_match
-    
-  end
-  
-  def format_output
-    
-  end
+
   
 
 end
